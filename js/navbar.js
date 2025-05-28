463660a4af711a9e38b2c941f483e7b36b8551d9
RaulGomezGarcia <rgomezgarcia1999@gmail.com>
2025-05-28 17:44:21 +0200

    Update navbar.js
    modificacion del navbar solo para githubs

--- js/navbar.js
+++ js/navbar.js
@@ -2,22 +2,22 @@ const linksBox = document.getElementById('links-box');
 const linksItem = [
     {
         title: "Inicio",
-        url: "../index.html"
+        url: "/Raul_Gomez_TrabajoJAVASCRIPT/" // Esto apunta a la raíz de tu proyecto en GitHub Pages (donde suele estar tu index.html)
     },
     {
         title: "Contacto",
-        url: "../views/contacto.html"
+        url: "/Raul_Gomez_TrabajoJAVASCRIPT/views/contacto.html"
     },
     {
         title: "Galeria",
-        url: "../views/galeria.html"
+        url: "/Raul_Gomez_TrabajoJAVASCRIPT/views/galeria.html"
     },
     {
         title: "Presupuesto",
-        url: "../views/presupuestos.html"
+        url: "/Raul_Gomez_TrabajoJAVASCRIPT/views/presupuestos.html"
     },
-]
+];
 
 linksItem.map((link) =>{
-    linksBox.innerHTML += `<a href="${link.url}">${link.title}</a>`
-})
+    linksBox.innerHTML += `<a href="${link.url}">${link.title}</a>`;
+});
